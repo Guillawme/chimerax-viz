9e3fe45140369011a705cddc3492902a80a01e5b
Guillaume Gaullier <guillaume@gaullier.org>
2021-10-05 10:44:15 +0200

    Fixed filenames so that volume series EMD-13070 opens in correct order

--- 7OTQ/download-EMD-13070-maps.sh
+++ 7OTQ/download-EMD-13070-maps.sh
@@ -7,7 +7,18 @@ rsync.ebi.ac.uk::pub/databases/emdb/structures/EMD-13070/ ./EMD-13070
 
 # Move main map to the directory containing the rest of the volume series
 # and give it a consistent filename
-mv ./EMD-13070/map/emd_13070.map.gz ./EMD-13070/other/emd_13070_additional_0.map.gz
+mv ./EMD-13070/map/emd_13070.map.gz ./EMD-13070/other/emd_13070_additional_00.map.gz
+
+# Zero-pad index of some files so they make up the volume series in correct order
+mv ./EMD-13070/other/emd_13070_additional_1.map.gz ./EMD-13070/other/emd_13070_additional_01.map.gz
+mv ./EMD-13070/other/emd_13070_additional_2.map.gz ./EMD-13070/other/emd_13070_additional_02.map.gz
+mv ./EMD-13070/other/emd_13070_additional_3.map.gz ./EMD-13070/other/emd_13070_additional_03.map.gz
+mv ./EMD-13070/other/emd_13070_additional_4.map.gz ./EMD-13070/other/emd_13070_additional_04.map.gz
+mv ./EMD-13070/other/emd_13070_additional_5.map.gz ./EMD-13070/other/emd_13070_additional_05.map.gz
+mv ./EMD-13070/other/emd_13070_additional_6.map.gz ./EMD-13070/other/emd_13070_additional_06.map.gz
+mv ./EMD-13070/other/emd_13070_additional_7.map.gz ./EMD-13070/other/emd_13070_additional_07.map.gz
+mv ./EMD-13070/other/emd_13070_additional_8.map.gz ./EMD-13070/other/emd_13070_additional_08.map.gz
+mv ./EMD-13070/other/emd_13070_additional_9.map.gz ./EMD-13070/other/emd_13070_additional_09.map.gz
 
 # Uncompress all maps, since ChimeraX cannot (yet?) read compressed maps
 # See <https://www.cgl.ucsf.edu/chimerax/docs/user/commands/open.html#compressed>
